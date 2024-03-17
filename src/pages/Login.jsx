5d2f48c2761bb274885bf9324949ac57b6219967
sbinwal <sandeep.binwal@escalesolutions.com>
2024-03-18 00:24:52 +0530

    completed e-commerce-project

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,77 +1,94 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import handleLogin from '../components/Auth/Login';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const [formData, setFormData] = useState({ email: '', password: '' });
-    const navigate = useNavigate();
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
 
-    // Changing the form fields value
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({ ...prevData, [name]: value }));
-      };
+  // Changing the form fields value
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
-      //Submitting the form
-      const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-          const token = await handleLogin(formData); //Call the handle Login function
-          localStorage.setItem("token",JSON.stringify(token)) //Storing token in local storae
-          navigate("/") //On Successful login navigate to homepage
+  // Toggling password visibility
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
 
-        } catch (error) {
-           console.log("error",error)
-        }
-      };
+  // Submitting the form
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const token = await handleLogin(formData); // Call the handle Login function
+      localStorage.setItem('token', JSON.stringify(token)); // Storing token in local storage
+      navigate('/'); // On Successful login navigate to homepage
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-    <div className="max-w-md w-full space-y-8">
-      <div>
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Log in to your account</h2>
-      </div>
-      <form className="" onSubmit={handleSubmit}>
-        <div className="rounded-md shadow-sm -space-y-px">
-          <div>
-            <input
-              id="email-address"
-              name="email"
-              type="email"
-              autoComplete="email"
-              required
-              className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-              placeholder="Email address"
-              value={formData.email}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className='mt-6'>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              autoComplete="current-password"
-              required
-              className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm mt-4"
-              placeholder="Password"
-              value={formData.password}
-              onChange={handleInputChange}
-            />
-          </div>
-        </div>
-
+      <div className="max-w-md w-full space-y-8">
         <div>
-          <button
-            type="submit"
-            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-4"
-          >
-            Log in
-          </button>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Log in to your account</h2>
         </div>
-      </form>
-    </div>
-  </div>
-  )
-}
+        <form className="" onSubmit={handleSubmit}>
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="mt-6 relative">
+              <input
+                id="password"
+                name="password"
+                type={showPassword ? 'text' : 'password'}
+                autoComplete="current-password"
+                required
+                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm mt-4"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+              <button
+                type="button"
+                className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 focus:outline-none"
+                onClick={togglePasswordVisibility}
+              >
+                {showPassword ? (
+               <img src='/images/openEye.svg' alt='open eye' className='h-3'/>
+                ) : (
+                  <img src='/images/closedEye.svg' alt='close eye' className='h-3'></img>
+                )}
+              </button>
+            </div>
+          </div>
 
-export default Login
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-4"
+            >
+              Log in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
